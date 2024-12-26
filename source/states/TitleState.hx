b143c214ac282d04321b7339a91fc9a22108d3f7
Rosa <97431838+SquidBowl@users.noreply.github.com>
2024-12-26 17:11:17 -0500

    outdated engine version implementation

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -236,7 +236,7 @@ class TitleState extends MusicBeatState
 		#if CHECK_FOR_UPDATES
 		if(ClientPrefs.data.checkForUpdates && !closedState) {
 			trace('checking for update');
-			var http = new haxe.Http("https://raw.githubusercontent.com/ShadowMario/FNF-PsychEngine/main/gitVersion.txt");
+			var http = new haxe.Http("https://raw.githubusercontent.com/SadeceNicat/Hybrid-Engine/Sad/gitVersion.txt");
 
 			http.onData = function (data:String)
 			{
